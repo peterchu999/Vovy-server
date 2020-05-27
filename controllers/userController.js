02ca7ba02089c86f54b009eb8645a1c4c895d6d5
peterchu999 <peter.andrew987@gmail.com>
2020-05-27 15:23:21 +0700

    add logging

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,7 @@ const {createUser, updateUser} = require('../services/userService')
 
 const makeUser = async (req, res) => {
     try{
+        console.log(res.body)
         const {uuid,deviceId, category = [], name = "not defined"} = req.body
         if(!uuid || !deviceId) {
             throw new Error("must have uuid and deviceId")
